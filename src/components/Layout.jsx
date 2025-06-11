2238da483ec45fe82daf889a678bb3378dfdc2ad
pippenberger <p.buhrmester@gmail.com>
2025-06-11 12:30:14 +0200

    Fix routing and navigation

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,22 +1,17 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
-import { Link } from 'react-router-dom';
 
-
-const Layout = () => {
-  return (
-    <div>
-      <nav style={{ marginBottom: '20px' }}>
-        <Link to="/">🏠 Home</Link> |{' '}
-        <Link to="/input">📥 Ergebnisse</Link> |{' '}
-        <Link to="/leaderboard">📊 Punktestand</Link>
-        <Link to="/">Start</Link> |{' '}
-  <Link to="/leaderboard">Leaderboard</Link> |{' '}
-  <Link to="/rules">Regelwerk</Link>
-      </nav>
-      <Outlet />
-    </div>
-  );
-};
+const Layout = () => (
+  <div>
+    <nav style={{ marginBottom: '20px' }}>
+      <Link to="/">🏠 Home</Link> |{' '}
+      <Link to="/input">📥 Ergebnisse</Link> |{' '}
+      <Link to="/leaderboard">📊 Punktestand</Link> |{' '}
+      <Link to="/rules">Regelwerk</Link>
+    </nav>
+    <Outlet />
+  </div>
+);
 
 export default Layout;
+
